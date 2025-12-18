e93498a00ee8907a6a17119d1481bcbea886aab1
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2025-12-18 17:49:40 +0100

    refactor: move component registrations to a dedicated file
    Simplifies `run.go` by extracting the `components` function to `components.go`.

--- internal/run/run.go
+++ internal/run/run.go
@@ -7,24 +7,7 @@ import (
 	"go.opentelemetry.io/collector/confmap/provider/fileprovider"
 	"go.opentelemetry.io/collector/confmap/provider/httpprovider"
 	"go.opentelemetry.io/collector/confmap/provider/yamlprovider"
-	"go.opentelemetry.io/collector/connector"
-	"go.opentelemetry.io/collector/exporter"
-	"go.opentelemetry.io/collector/exporter/debugexporter"
-	"go.opentelemetry.io/collector/exporter/otlpexporter"
-	"go.opentelemetry.io/collector/exporter/otlphttpexporter"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/healthcheckextension"
-	"go.opentelemetry.io/collector/extension"
-	"go.opentelemetry.io/collector/extension/zpagesextension"
 	"go.opentelemetry.io/collector/otelcol"
-	"go.opentelemetry.io/collector/processor"
-	"go.opentelemetry.io/collector/processor/batchprocessor"
-	"go.opentelemetry.io/collector/processor/memorylimiterprocessor"
-	"go.opentelemetry.io/collector/receiver"
-	"go.opentelemetry.io/collector/receiver/otlpreceiver"
-	"go.opentelemetry.io/collector/service/telemetry/otelconftelemetry"
-
-	"github.com/mikluko/opentelemetry-collector-nats/internal/natsexporter"
-	"github.com/mikluko/opentelemetry-collector-nats/internal/natsreceiver"
 )
 
 func Main(version string) error {
@@ -51,58 +34,4 @@ func Main(version string) error {
 
 	cmd := otelcol.NewCommand(set)
 	return cmd.Execute()
-}
-
-func components() (otelcol.Factories, error) {
-	var err error
-	factories := otelcol.Factories{}
-
-	// Extensions
-	factories.Extensions, err = otelcol.MakeFactoryMap[extension.Factory](
-		healthcheckextension.NewFactory(),
-		zpagesextension.NewFactory(),
-	)
-	if err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	// Receivers
-	factories.Receivers, err = otelcol.MakeFactoryMap[receiver.Factory](
-		otlpreceiver.NewFactory(),
-		natsreceiver.NewFactory(),
-	)
-	if err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	// Exporters
-	factories.Exporters, err = otelcol.MakeFactoryMap[exporter.Factory](
-		debugexporter.NewFactory(),
-		otlpexporter.NewFactory(),
-		otlphttpexporter.NewFactory(),
-		natsexporter.NewFactory(),
-	)
-	if err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	// Processors
-	factories.Processors, err = otelcol.MakeFactoryMap[processor.Factory](
-		batchprocessor.NewFactory(),
-		memorylimiterprocessor.NewFactory(),
-	)
-	if err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	// Connectors (empty for now, but required)
-	factories.Connectors, err = otelcol.MakeFactoryMap[connector.Factory]()
-	if err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	// Telemetry
-	factories.Telemetry = otelconftelemetry.NewFactory()
-
-	return factories, nil
 }
